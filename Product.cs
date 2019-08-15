d7c5147e137c8be3d66d39692a1fea6479acb0e9
ratto <rattopedro@gmail.com>
2019-08-15 12:36:00 -0300

    Class Product created; Making the program read data from origin.csv file

---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathExercise
+{
+    class Product
+    {
+        private string Name { get; set; }
+        private double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product (string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace PathExercise
 {
@@ -17,16 +18,31 @@ namespace PathExercise
         static void Main(string[] args)
         {
             string originPath = @"d:\Temp\PathExercise";
+            FileStream fs = null;
+            StreamReader sr = null;
 
             try
             {
+                // let's open the origin file
+                fs = new FileStream(originPath + @"\origin.csv", FileMode.Open);
+                sr = new StreamReader(fs);
 
+                // the file has some lines, so let's print them on screen to check if everything is working as expected
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             }
             catch (IOException e)
             {
                 // IO error exception message
                 Console.WriteLine("An error ocurred!");
                 Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
             }
         }
     }

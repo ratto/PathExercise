2748023bbe971f7b33a227d458d3a4f2f11126b7
ratto <rattopedro@gmail.com>
2019-08-17 15:00:28 -0300

    Exercise done!

--- Program.cs
+++ Program.cs
@@ -19,91 +19,131 @@ namespace PathExercise
         static void Main(string[] args)
         {
             string originPath = @"d:\Temp\PathExercise";
-            FileStream fs = null;
-            StreamReader sr = null;
+            Directory.CreateDirectory(originPath + @"\out");
+            string targetFile = originPath + @"\out\summary.csv";
+
             List<string> lines = new List<string>();
+            List<string> entries = new List<string>();
 
             try
             {
-                // let's open the origin file
-                fs = new FileStream(originPath + @"\origin.csv", FileMode.Open);
-                sr = new StreamReader(fs);
-                
-
-
-                double price = 0.00;
-                int quantity = 0;
                 char currentChar = '0';
 
-                // We read the file and store each of it's line in lines list
-                while (!sr.EndOfStream)
+                using (StreamReader srOrigin = File.OpenText(originPath + @"\origin.csv"))
                 {
-                    string line = sr.ReadLine() + ",";
-                    lines.Add(line);
-                }
-
-                // Now we read each stored line and proccess them accordingly
-                Console.WriteLine("--- DATA PROCCESSING ---");
-                Console.WriteLine();
-
-                foreach (string line in lines)
-                {
-                    Console.WriteLine(line);
-
-                    Product product = new Product();
-
-                    // variables needed to split each data from origin file
-                    string[] words = new string[3];
-                    int charAt = 0;
-                    string word = null;
-                    
-                    // In here we split the whole line into data for later proccessing
-                    do
+                    // We read the file and store each of it's line in lines list
+                    while (!srOrigin.EndOfStream)
                     {
-                        currentChar = line[charAt];
-                        if (currentChar != ',')
+                        // I insert this ',' in the end of each line so the processor can read the last data
+                        string line = srOrigin.ReadLine() + ",";
+                        lines.Add(line);
+                    }
+
+                    // Now we read each stored line and proccess them accordingly
+                    Console.WriteLine("--- DATA PROCCESSING ---");
+                    Console.WriteLine();
+
+                    foreach (string line in lines)
+                    {
+                        Product product = new Product();
+
+                        // variables needed to split each data from origin file
+                        string[] words = new string[3];
+                        int charAt = 0;
+                        string word = null;
+
+                        // In here we split the whole line into data for later proccessing
+                        do
                         {
-                            word += currentChar;
-                            charAt++;
-                        }
-                        else
-                        {
-                            if (words[0] == null)
+                            currentChar = line[charAt];
+                            if (currentChar != ',')
                             {
-                                words[0] = word;
-                                word = null;
-                            }
-                            else if (words[1] == null)
-                            {
-                                words[1] = word;
-                                word = null;
+                                word += currentChar;
+                                charAt++;
                             }
                             else
                             {
-                                words[2] = word;
-                                word = null;
+                                if (words[0] == null)
+                                {
+                                    words[0] = word;
+                                    word = null;
+                                }
+                                else if (words[1] == null)
+                                {
+                                    words[1] = word;
+                                    word = null;
+                                }
+                                else
+                                {
+                                    words[2] = word;
+                                    word = null;
+                                }
+
+                                charAt++;
                             }
-
-                            charAt++;
                         }
+                        while (charAt < line.Length);
+
+                        // storing data in product
+                        product.Name = words[0];
+                        product.Price = double.Parse(words[1], CultureInfo.InvariantCulture);
+                        product.Quantity = int.Parse(words[2]);
+
+                        Console.WriteLine(product);
+
+                        // storing this product in a new entry
+                        entries.Add(product.Name + "," + product.getTotalPrice().ToString("F2", CultureInfo.InvariantCulture));
                     }
-                    while (charAt < line.Length);
-
-                    product.Name = words[0];
-                    product.Price = double.Parse(words[1], CultureInfo.InvariantCulture);
-                    product.Quantity = int.Parse(words[2]);
-
-                    Console.WriteLine(product.getTotalPrice().ToString("F2", CultureInfo.InvariantCulture));
-                    Console.WriteLine(product);
-                    Console.WriteLine();
                 }
 
+                Console.WriteLine();
                 Console.WriteLine("--- END OF DATA PROCCESSING ---");
+                Console.WriteLine();
+                
+                // writing each entry inside 'summary.csv' file
+                using (StreamWriter swTarget = File.AppendText(targetFile))
+                {
+                    foreach (string entry in entries)
+                    {
+                        swTarget.WriteLine(entry);
+                    }
+                }
+
+                Console.WriteLine("Write ok!"); // output to check if the file was written
+                Console.WriteLine();
+
+                Console.WriteLine("--- OUTPUT (summary.csv) ---");
+                Console.WriteLine();
+
+                // and then we print the information from 'summary.csv' file
+                using (StreamReader srTarget = File.OpenText(targetFile))
+                {
+                    while (!srTarget.EndOfStream)
+                    {
+                        Console.WriteLine(srTarget.ReadLine());
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("--- END OF FILE ---");
+
                 /*
                  * Right now it is just printing onscreen:
-                 * Input (with a ',' in the end, to make the data process easier)
+                 * --- DATA PROCCESSING ---
+                 * 
                  * Name -> price x quantity = total amount (in BRL)
+                 * 
+                 * --- END OF DATA PROCCESSING ---
+                 * 
+                 * Write ok!
+                 * 
+                 * --- OUTPUT (summary.csv) ---
+                 * 
+                 * Name,Total vallue
+                 * 
+                 * --- END OF FILE ---
                  */
+
             }
             catch (IOException e)
             {
@@ -113,7 +153,7 @@ namespace PathExercise
             }
             finally
             {
-                if (sr != null) sr.Close();
+                
             }
         }
     }
